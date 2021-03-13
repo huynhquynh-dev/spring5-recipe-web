cb256ddfdaeed48d48d0e12bb94cdf0897ab1854
huynhquynh-dev <huynhquynh-dev>
2021-03-13 22:05:22 +0700

    first project

---
+++ src/main/java/com/lionhuynh/Spring5RecipeWebApplication.java
@@ -0,0 +1,13 @@
+package com.lionhuynh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lionhuynh/Spring5RecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionhuynh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
