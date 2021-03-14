4e9267a558022640992bc5c6ece8f89e80064799
huynhquynh-dev <huynhquynh-dev>
2021-03-14 21:04:42 +0700

    Added Specialties Map Service

---
+++ src/main/java/com/lionhuynh/service/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.lionhuynh.service;
+
+import com.lionhuynh.model.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality, Long> {
+}
---
+++ src/main/java/com/lionhuynh/service/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.lionhuynh.service.map;
+
+import com.lionhuynh.model.Speciality;
+import com.lionhuynh.service.SpecialitesService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
